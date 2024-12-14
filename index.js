21e62d063fd39c0c00e11b75f3eb9c70e8a215dd
Etesh1212 <166606783+Etesh1212@users.noreply.github.com>
2024-12-14 20:36:23 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,19 @@
+const month=document.querySelector(".month");
+const weekday=document.querySelector(".weekday");
+const day=document.querySelector(".day");
+const year=document.querySelector(".year");
+
+
+window.addEventListener("DOMContentLoaded",()=>{
+    const date=new Date();
+    console.log(date,"date")
+    console.log("month",date.getMonth())
+    console.log("day",date.getDate())
+    console.log("year",date.getFullYear())
+    day.textContent=date.gerDate();
+    year.textContent=date.getFullYear();
+    month.textContent=date.toLocaleDateString("en-US",{month:"long"})
+    weekday.textContent=date.toLocaleDateString("en-US",{weekday:"long"})
+
+})
+
